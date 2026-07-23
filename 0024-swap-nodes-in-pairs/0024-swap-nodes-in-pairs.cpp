7e12a0c95c9e76b3e44f3670fb7a4bb72a01687f
Risabh Pandey <2k24.csds1c.2414409@gmail.com>
2026-07-23 14:27:37 +0530

    Time: 0 ms (100%), Space: 11.1 MB (58.4%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -8,21 +8,25 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        ListNode* curr = head;
-
-        while(curr != NULL && curr->next != NULL){
-            ListNode* first = curr;
-            ListNode* second = curr->next;
-
-            int t= first->val;
-            first->val = second->val;
-            second->val = t;
-
-            curr = second->next;
+        // Base Case
+        if (head == NULL || head->next == NULL) {
+            return head;
         }
-        return head;
+
+        ListNode* first = head;
+        ListNode* second = head->next;
+
+        // Recursively swap the remaining list
+        first->next = swapPairs(second->next);
+
+        // Swap the current pair
+        second->next = first;
+
+        // New head of this pair
+        return second;
     }
 };
