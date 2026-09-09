4d192e69f817c556a0339eedb72fcf87f3323287
Risabh Pandey <2k24.csds1c.2414409@gmail.com>
2026-09-09 18:37:36 +0530

    Time: 2 ms (13.79%), Space: 9.5 MB (80.56%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.cpp
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int maxPower(string s) {
-        int curr =1;
+        int count = 1; 
         int maxPow = 1;
 
-        for(int i=1; i<s.size();i++){
+        for(int i=1; i< s.size(); i++){
             if(s[i-1] == s[i]){
-                curr++;
+                count++ ;
             }
             else{
-                curr =1;
+                count = 1;
             }
-            maxPow = max(maxPow, curr); 
-        }
+            maxPow = max(maxPow, count);
+        } 
         return maxPow;
     }
 };
