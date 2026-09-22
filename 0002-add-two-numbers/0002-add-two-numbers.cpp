9eb3fcea7f4fb4ae66c7beafa7ece7cad54dbef0
Risabh Pandey <2k24.csds1c.2414409@gmail.com>
2026-09-22 19:44:25 +0530

    Time: 3 ms (34.37%), Space: 77.2 MB (12.76%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,35 +11,30 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
         ListNode* dummy = new ListNode(-1);
         ListNode* ans = dummy;
-        ListNode* ptr1 = l1;
-        ListNode* ptr2 = l2;
-        int carry = 0;
-        
-        while(ptr1 != NULL || ptr2 != NULL){
-            int val1 = ptr1 != NULL ? ptr1->val :0;
-            int val2 = ptr2 != NULL ? ptr2->val :0;
+        ListNode*ptr1 = l1;
+        ListNode*ptr2 = l2;
+        int carry =0;
+        while(ptr1 != NULL || ptr2 != NULL || carry != 0){
+            int sum = carry;
 
-            int sum =val1+ val2 +carry;
-            int d = sum%10;
+            if(ptr1 != NULL){
+                sum += ptr1->val;
+                ptr1 = ptr1->next;
+            }
+            if(ptr2 != NULL){
+                sum += ptr2->val;
+                ptr2 = ptr2->next;
+            }
+            
+            int digit = sum%10;
             carry = sum/10;
 
-            ListNode* temp = new ListNode(d);
-            dummy->next = temp;
-            dummy = dummy->next;
-
-            ptr1 = ptr1 !=NULL ? ptr1->next : NULL;
-            ptr2 = ptr2 !=NULL ? ptr2->next : NULL;
-        }
-        if(carry >0){
-            ListNode* temp = new ListNode(carry);
+            ListNode* temp = new ListNode(digit);
             dummy->next = temp;
             dummy = dummy->next;
         }
         return ans->next;
-            
     }
 };
